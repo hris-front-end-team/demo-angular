6cee3c469c04bd35fb635e6373f8fe26c03662c0
Igorek <Igor.Soloydenko@seattle.gov>
2017-09-08 11:14:32 -0700

    Add header-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+// imports are for TypeScript language
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
+  // declarations are used by Angular compiler (run time) when compiling templates
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
