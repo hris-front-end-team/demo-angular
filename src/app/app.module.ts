1831646b4ba62f00a6bf11917f0150ac32e5ed40
Igorek <Igor.Soloydenko@seattle.gov>
2017-09-08 11:40:40 -0700

    Add high level router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { OtherPageComponent } from './other-page/other-page.component';
+import { appRoutes } from "app/app/app-routing.module";
+import { RouterModule } from "@angular/router";
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   // declarations are used by Angular compiler (run time) when compiling templates
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    OtherPageComponent,
+    MainPageComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { AppComponent } from '../app.component';
+import { OtherPageComponent } from '../other-page/other-page.component';
+import { MainPageComponent } from '../main-page/main-page.component';
+
+export const appRoutes: Routes = [
+  { path: 'special', redirectTo: 'other' },
+
+  { path: 'main', component: MainPageComponent },
+  { path: 'other', component: OtherPageComponent },
+
+  { path: '**', component: MainPageComponent, },
+];
